578fe0ff3bf2f8ed83f0b34fc0d9b6a818c9b717
Srikanth <nandiraju@yahoo.com>
2026-02-24 19:27:52 -0800

    fix: switch to HashRouter for GitHub Pages SPA compatibility

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import {ThemeProvider} from './context/ThemeContext';
 import App from './App.tsx';
 import './index.css';
@@ -8,9 +8,9 @@ import './index.css';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </ThemeProvider>
   </StrictMode>,
 );
